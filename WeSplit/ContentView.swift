c6427ba0c7707f6052b5fc461a818e64c3aa787c
Woodrow Martyr <woodrow.martyr@gmail.com>
2023-12-21 06:40:13 +1030

    End of Project 1 Part 2

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,18 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let students = ["Harry", "Hermione", "Ron"]
-    @State private var selectedStudent = "Harry"
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused:Bool
+    
+    let tipPercentages = [0, 10, 15, 20, 25]
+    
+    var tipPerPerson: Double {
+        let actualNumberOfPeople = Double(numberOfPeople + 2)
+        let finalAmount = checkAmount * (100 + Double(tipPercentage)) / 100
+        
+        return finalAmount / actualNumberOfPeople
+    }
     
     var body: some View {
         NavigationStack {
             Form {
-                Picker("Select your student", selection: $selectedStudent) {
-                    ForEach(students, id: \.self) {
-                        Text($0)
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) People")
+                        }
+                    }
+                    //.pickerStyle(.navigationLink)
+                }
+                Section("How much do you want to tip?") {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section {
+                    Text(tipPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
                     }
                 }
-                Text("Student Picked: \(selectedStudent)")
             }
         }
     }
